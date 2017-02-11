78258de0b01e3ac27b7768830dbfd79069e95bac
Axel Nennker <axel.nennker@telekom.de>
2017-02-11 14:23:30 +0100

    script to generate raw text and html version

---
+++ xml2rfc.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+xml2rfc draft-hill-delegated-recovery.xml --text -o draft-hill-delegated-recovery.raw.txt
+xml2rfc draft-hill-delegated-recovery.xml --html -o draft-hill-delegated-recovery.html
